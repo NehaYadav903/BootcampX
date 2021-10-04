d120bf96c2910457acf8f1e255b8ed0199c8946c
Neha Yadav <nehayadav903@gmail.com>
2021-10-04 17:05:30 -0400

    Add the comment

--- 2_queries_joins/2_cohorts_total_assignment_duration.sql
+++ 2_queries_joins/2_cohorts_total_assignment_duration.sql
@@ -1,3 +1,5 @@
+-- Get the total amount of time that all students from a specific cohort have spent on all assignments.
+
 SELECT sum(assignment_submissions.duration) as total_duration
 FROM assignment_submissions
 JOIN students ON students.id = student_id
